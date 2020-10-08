5af1cec07202153d881aabe2cca42d6e33443c9e
Artem <artyom.mihailovich@gmail.com>
2020-10-08 17:46:41 +0700

    Implementing Gallery framework

--- ChatApp/EditProfileTableViewController.swift
+++ ChatApp/EditProfileTableViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Gallery
+import ProgressHUD
 
 class EditProfileTableViewController: UITableViewController {
 
@@ -14,6 +16,11 @@ class EditProfileTableViewController: UITableViewController {
     @IBOutlet weak var avatarImageView: UIImageView!
     @IBOutlet weak var statusLabel: UILabel!
     @IBOutlet weak var usernameTextField: UITextField!
+    
+    
+    //MARK: - Variables
+    
+    var gallery: GalleryController!
     
     
     //MARK: - View life cycle
@@ -55,6 +62,15 @@ class EditProfileTableViewController: UITableViewController {
         //TODO: Show status view
     }
     
+    
+    //MARK: - IBActions
+    
+    
+    @IBAction func editButtonPressed(_ sender: Any) {
+        
+        showImageGallery()
+    }
+    
     //MARK: - Update UI
     
     private func showUserInfo() {
@@ -72,11 +88,27 @@ class EditProfileTableViewController: UITableViewController {
     
     //MARK: - Configure
     
-    func configureTextField(){
+    func configureTextField() {
         usernameTextField.delegate = self
         usernameTextField.clearButtonMode = .whileEditing
     }
+    
+    
+    //MARK: - Gallery
+
+    func showImageGallery() {
+        self.gallery = GalleryController()
+        self.gallery.delegate = self
+        
+        Config.tabsToShow = [.imageTab, .cameraTab]
+        Config.Camera.imageLimit = 1
+        Config.initialTab = .imageTab
+        
+        self.present(gallery, animated: true, completion: nil)
+    }
 }
+
+
 
 
     //MARK: - Extension
@@ -98,5 +130,35 @@ extension EditProfileTableViewController: UITextFieldDelegate {
             return false
         }
         return true
+    }
+}
+
+extension EditProfileTableViewController: GalleryControllerDelegate {
+    
+    func galleryController(_ controller: GalleryController, didSelectImages images: [Image]) {
+        
+        if images.count > 0 {
+            images.first!.resolve { (avatarImage) in
+                if avatarImage != nil {
+                    //TODO: upluad image
+                    self.avatarImageView.image = avatarImage
+                } else {
+                    ProgressHUD.showError("Couldn't select something.")
+                }
+            }
+        }
+        controller.dismiss(animated: true, completion: nil)
+    }
+    
+    func galleryController(_ controller: GalleryController, didSelectVideo video: Video) {
+        controller.dismiss(animated: true, completion: nil)
+    }
+    
+    func galleryController(_ controller: GalleryController, requestLightbox images: [Image]) {
+        controller.dismiss(animated: true, completion: nil)
+    }
+    
+    func galleryControllerDidCancel(_ controller: GalleryController) {
+        controller.dismiss(animated: true, completion: nil)
     }
 }
