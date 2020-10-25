97fcccbe6f638f3da3b7907b4c9547c1077b4da7
Artem <artyom.mihailovich@gmail.com>
2020-10-25 08:52:46 +0700

    Downloading and search users and refresh control

--- ChatApp/FirebaseHelpers/FirebaseUserListener.swift
+++ ChatApp/FirebaseHelpers/FirebaseUserListener.swift
@@ -133,4 +133,54 @@ class FirebaseUserListener {
             }
         }
     }
+    
+    func downloadAllUsersFromFirebase(completion: @escaping(_ allUsers: [User]) -> Void) {
+        var users: [User] = []
+        
+        firebaseReference(.User).limit(to: 500).getDocuments { (querySnapshot, error) in
+            guard let document = querySnapshot?.documents else {
+                print("ERROR: - No documents in all users!")
+                
+                return
+            }
+            let allUsers = document.compactMap {(queryDocumentSnapshot) -> User? in
+                
+            return try? queryDocumentSnapshot.data(as: User.self)
+            }
+            
+            for user in allUsers {
+                
+                if User.currentId != user.id {
+                    users.append(user)
+                }
+            }
+            
+            completion(users)
+        }
+    }
+    
+    func downloadUsersFromFirebase(withIds: [String], completion: @escaping(_ allUsers: [User]) -> Void) {
+        var count = 0
+        var userArray: [User] = []
+        
+        for userId in withIds {
+            firebaseReference(.User).document(userId).getDocument { (querySnapshot, error) in
+                
+                guard let document = querySnapshot else {
+                    print("ERROR: - No document for user!")
+                    
+                    return
+                }
+                
+                let user = try? document.data(as: User.self)
+                
+                userArray.append(user!)
+                count += 1
+                
+                if count == withIds.count {
+                    completion(userArray)
+                }
+            }
+        }
+    }
 }
--- ChatApp/UsersTableViewController.swift
+++ ChatApp/UsersTableViewController.swift
@@ -21,10 +21,23 @@ class UsersTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //Refresh controller
+        self.refreshControl = UIRefreshControl()
+        self.tableView.refreshControl = self.refreshControl
         
-//        createDummyUsers()
+        
+        tableView.tableFooterView = UIView()
+        setupSearchController()
+        downloadUsers()
+          //createDummyUsers()
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        navigationItem.largeTitleDisplayMode = .always
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
     
     // MARK: - Table view data source
 
@@ -40,5 +53,76 @@ class UsersTableViewController: UITableViewController {
         cell.configure(user: user)
         
         return cell
+    }
+    
+    
+    //MARK: - TableView delegate
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView()
+        headerView.backgroundColor = UIColor(named: "TableViewBackgroundColor")
+        
+        return headerView
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        //TODO: - Show profile view
+    }
+    
+    
+    //MARK: - Download users
+    
+    private func downloadUsers() {
+        FirebaseUserListener.shared.downloadAllUsersFromFirebase { (allFirebaseUsers) in
+            self.allUsers = allFirebaseUsers
+            
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
+    
+    //MARK: - Setup search controller
+    
+    private func setupSearchController() {
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = true
+        
+        searchController.searchBar.placeholder = "Search user"
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchResultsUpdater = self
+        definesPresentationContext = true
+    }
+    
+    private func filteredContentForSearchText(searchText: String) {
+        filteredUsers = allUsers.filter({ (user) -> Bool in
+            return user.username.lowercased().contains(searchText.lowercased())
+        })
+        tableView.reloadData()
+    }
+    
+    
+    //MARK: - UIScrollView delegate
+
+    override func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        print("Refresh")
+        
+         if self.refreshControl!.isRefreshing {
+            self.downloadUsers()
+            self.refreshControl!.endRefreshing()
+        }
+    }
+}
+
+extension UsersTableViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        filteredContentForSearchText(searchText: searchController.searchBar.text!)
     }
 }
