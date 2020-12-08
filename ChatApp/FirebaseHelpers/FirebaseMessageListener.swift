634349f0156eaf439de233283b3692ad9d2203e8
Artem <artyom.mihailovich@gmail.com>
2020-12-08 13:56:59 +0700

    Listening for new chats, new chats test run

--- ChatApp/FirebaseHelpers/FirebaseMessageListener.swift
+++ ChatApp/FirebaseHelpers/FirebaseMessageListener.swift
@@ -13,7 +13,73 @@ class FirebaseMessageListener  {
     
     static let shared = FirebaseMessageListener()
     
+    //MARK: - Variables
+    
+    var newChatListener: ListenerRegistration!
+    var updateChatListener: ListenerRegistration!
+    
+    
     private init() {}
+    
+    
+    //MARK: - Listen for new chats
+    
+    func listenForNewChats(_ documentId: String, collectionId: String, lastMessageDate: Date) {
+        newChatListener = firebaseReference(.Messages).document(documentId).collection(collectionId).whereField(kDATE, isGreaterThan: lastMessageDate).addSnapshotListener({ (querySnapshot, error) in
+            
+            guard let snapShot = querySnapshot else { return }
+            
+            for change in snapShot.documentChanges {
+                if change.type == .added {
+                    
+                    let result = Result {
+                        
+                        try? change.document.data(as: LocalMessage.self)
+                    }
+                    
+                    switch result {
+                    case .success(let messageObject):
+                        if let message = messageObject {
+                            RealmManager.shared.saveToRealm(message)
+                        } else {
+                            print("Document does not exist!")
+                        }
+                    case .failure(let error):
+                        print("Error docoding local chat: \(error.localizedDescription)")
+                    }
+                }
+            }
+        })
+    }
+   
+    
+    //MARK: - Check for old chats
+    
+    func checkForOldChats(_ documentId: String, collectionId: String) {
+        
+        // Firebase part
+        
+        firebaseReference(.Messages).document(documentId).collection(collectionId).getDocuments { (querySnapshot, error) in
+            
+            guard let documents = querySnapshot?.documents else {
+                print("No documents for old chats")
+                return
+            }
+            
+            var oldMessages = documents.compactMap { (queryDocumentSnapshot) -> LocalMessage? in
+                return try? queryDocumentSnapshot.data(as: LocalMessage.self)
+            }
+            
+            oldMessages.sort(by: {$0.date < $1.date})
+            
+            //Realm part
+            
+            for message in oldMessages {
+                RealmManager.shared.saveToRealm(message)
+            }
+        }
+    }
+    
     
     //MARK: - Add, Update, Delete
     
